84f5528967d034f4e91ba360eacede46ca508534
mink-kor <kims@huntington.edu>
2026-01-16 09:50:26 -0500

    Add files via upload

---
+++ Planning 260116.py
@@ -0,0 +1,18 @@
+# Swimming_pool.py
+# January 16, 2026
+# Min
+# Swimming Pool
+
+# input
+Length = int( input("Enter your Length: ") )
+Width = int( input("Enter Width: ") )
+Depth = int( input("Enter Depth: ") )
+
+# processing
+total_volume = Length * Width * Depth
+total_gallons = total_volume * (7.5)
+number_of_trips = total_volume / (4000)
+
+# output
+
+print( f"total gallons of water = {total_gallons:.2f} total_gallons" )
